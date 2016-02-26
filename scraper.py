52e91e535c1d88d1917ac9bc6bb93c58e7db7daa
Wilbert Lam <wilbertthelam@gmail.com>
2016-02-26 11:22:31 -0800

    added baseball/basketball stats scraping

--- scraper.py
+++ scraper.py
@@ -4,12 +4,34 @@
 import requests
 import bs4
 import urlparse
+import re
 
-playersDict = {}
+playersNameDict = {} # key: playerId, value: teamName
+teamDict = {} # key: playerId, value: dict with stats for each category
 
 def stripId(url):
 	parsed = urlparse.urlparse(url)
 	return urlparse.parse_qs(parsed.query)['teamId'][0]
+
+def baseballStatsObjCreator(statsDOM):
+	stats = {}
+
+	statHeaders = ['R', 'HR', 'RBI', 'SB', 'OBP', 'K', 'W', 'S', 'ERA', 'WHIP']
+
+	for i in range(0, len(statHeaders)):
+		stats[statHeaders[i]] = statsDOM[i].string
+
+	return stats
+
+def basketballStatsObjCreator(statsDOM):
+	stats = {}
+
+	statHeaders = ['FG', 'FT', '3PM', 'RB', 'AST', 'STL', 'BLK', 'PTS']
+
+	for i in range(0, len(statHeaders)):
+		stats[statHeaders[i]] = statsDOM[i].string
+
+	return stats
 
 # URL we want to scrape from
 scrapeURL = 'http://games.espn.go.com/flb/scoreboard?leagueId=44067&seasonId=2016'
@@ -20,17 +42,23 @@ response = requests.get(scrapeURL)
 # create DOM tree using BeautifulSoup
 soup = bs4.BeautifulSoup(response.content, 'lxml')
 
-team = {}
-teamInfo = soup.select(".linescoreTeamRow .teamName a[title]")
 
+teamInfo = soup.select(".linescoreTeamRow")
 
 for t in teamInfo:
-	teamId = stripId(t['href'])
-	teamName = t['title']
+	t_team = t.select(".teamName a[title]")
+	teamId = stripId(t_team[0]['href'])
+	teamName = t_team[0]['title']
 
-	playersDict[teamId] = teamName
+	t_stats = t.findAll('td', id=re.compile('^ls_tmTotalStat_'))
 
-for key, value in playersDict.iteritems():
+	playersNameDict[teamId] = teamName
+	teamDict[teamId] = baseballStatsObjCreator(t_stats)
+
+for key, value in playersNameDict.iteritems():
+	print key, value
+
+for key, value in teamDict.iteritems():
 	print key, value
 
 
